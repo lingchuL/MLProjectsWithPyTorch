46229507735b9194fae1d637538bfcf99a9fbe74
lingchuL <2938843848@qq.com>
2022-05-25 14:15:10 +0800

    Fashion MNIST Trained Data

--- NeuralNetworkPyTorchWay.py
+++ NeuralNetworkPyTorchWay.py
@@ -11,17 +11,17 @@ import matplotlib.pyplot as plt
 ComputeDevice = "cuda" if torch.cuda.is_available() else "cpu"
 
 training_data = torchvision.datasets.FashionMNIST(
-	root="data",
-	train=True,
-	download=False,
-	transform=torchvision.transforms.ToTensor(),
+    root="data",
+    train=True,
+    download=False,
+    transform=torchvision.transforms.ToTensor(),
 )
 
 test_data = torchvision.datasets.FashionMNIST(
-	root="data",
-	train=False,
-	download=False,
-	transform=torchvision.transforms.ToTensor(),
+    root="data",
+    train=False,
+    download=False,
+    transform=torchvision.transforms.ToTensor(),
 )
 
 batch_size = 64
@@ -32,73 +32,114 @@ test_dataloader = DataLoader(test_data, batch_size=batch_size)
 i_historyList = []
 J_historyList = []
 
+labelType = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
 
 class NeuralNetworkModel(torch.nn.Module):
-	def __init__(self):
-		super(NeuralNetworkModel, self).__init__()
+    def __init__(self):
+        super(NeuralNetworkModel, self).__init__()
 
-		self.LayerFunc = torch.nn.Linear(1, 1, device=ComputeDevice)
-		self.flatten = nn.Flatten()
-		self.linear_relu_stack = nn.Sequential(
-			nn.Linear(28 * 28, 512),
-			nn.ReLU(),
-			nn.Linear(512, 512),
-			nn.ReLU(),
-			nn.Linear(512, 10)
-		)
+        self.LayerFunc = torch.nn.Linear(1, 1, device=ComputeDevice)
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
 
-	def forward(self, x: torch.tensor) -> torch.tensor:
-		x = self.flatten(x)
-		logits = self.linear_relu_stack(x)
-		return logits
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
 
 def getLoss(y_pred, y):
-	LossFunc = torch.nn.CrossEntropyLoss()
-	J = LossFunc(y_pred, y)
-	return J
+    LossFunc = torch.nn.CrossEntropyLoss()
+    J = LossFunc(y_pred, y)
+    return J
 
 
-def train(dataloader, model, optimizer, iternum):
-	for epoch in range(iternum):
-		for batch, (X, y) in enumerate(dataloader):
-			X, y = X.to(ComputeDevice), y.to(ComputeDevice)
-			pred = model(X)
-			loss = getLoss(pred, y)
+def train(dataloader, model, lossfunc, optimizer):
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(ComputeDevice), y.to(ComputeDevice)
+        pred = model(X)
+        loss = lossfunc(pred, y)
 
-			optimizer.zero_grad()
-			loss.backward()
-			optimizer.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-			J_historyList.append(loss.detach().item())
-			i_historyList.append(epoch)
-
-
-def test(dataloader, model):
-	size = len(dataloader.dataset)
-	num_batches = len(dataloader)
-	model.eval()
-	test_loss, correct = 0, 0
-	with torch.no_grad():
-		for X, y in dataloader:
-			X, y = X.to(ComputeDevice), y.to(ComputeDevice)
-			pred = model(X)
-			test_loss += getLoss(pred, y).item()
-			correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-	test_loss /= num_batches
-	correct /= size
-	print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        J_historyList.append(loss.detach().item())
+        i_historyList.append(epoch)
 
 
-Model = NeuralNetworkModel()
-Model.to(device=ComputeDevice)
+def test(dataloader, model, lossfunc):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(ComputeDevice), y.to(ComputeDevice)
+            pred = model(X)
+            test_loss += lossfunc(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-Optimizer = torch.optim.SGD(Model.parameters(), lr=1e-3)
 
-train(train_dataloader, Model, Optimizer, 10)
+if __name__ == "__main__":
+    Model = NeuralNetworkModel()
+    Model.to(device=ComputeDevice)
 
-torch.save(Model.state_dict(), r".\Model.pth")
+    Model.load_state_dict(torch.load(r".\FashionMNISTModel.pth"))
 
-plt.figure(1)
-plt.plot(i_historyList, J_historyList)
-plt.show()
+    LossFunc = torch.nn.CrossEntropyLoss()
+    Optimizer = torch.optim.SGD(Model.parameters(), lr=1e-3)
+
+    '''
+    iternum = 5
+
+    for epoch in range(iternum):
+        train(train_dataloader, Model, LossFunc, Optimizer)
+        test(test_dataloader, Model, LossFunc)
+
+    torch.save(Model.state_dict(), r".\FashionMNISTModel.pth")
+
+
+    plt.figure(1)
+    plt.plot(i_historyList, J_historyList)
+    plt.show()
+    '''
+
+    with torch.no_grad():
+        for X, y in test_dataloader:
+            X, y = X.to(ComputeDevice), y.to(ComputeDevice)
+
+            pred = Model(X)
+            for i in range(64):
+                print(f"-------------------{i}----------------------")
+                print("It might be: ", labelType[pred.argmax(1)[i]])
+                print("It actually is: ", labelType[y[i]])
+                print("------------------------------------------")
+
+                plt.clf()
+                plt.imshow(np.reshape(X[i][0].cpu().numpy(), (28, 28)))
+                plt.draw()
+                plt.pause(1.5)
+
+            break
